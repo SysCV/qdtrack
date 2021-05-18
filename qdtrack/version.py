c7860f622287ed24c7a765335b76444d4cf3e731
Tobias Fischer <36965290+tobiasfshr@users.noreply.github.com>
2021-05-18 18:56:23 +0200

    fix setup.py when requirements.txt contains git repo (#42)

--- qdtrack/version.py
+++ qdtrack/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Mon Sep  7 10:37:20 2020
-__version__ = '0.1.0+f0ca1b0'
+# TIME: Tue May 18 18:09:29 2021
+__version__ = '0.1.0+57681d5'
 short_version = '0.1.0'
 version_info = (0, 1, 0)
